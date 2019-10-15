9ab3326dd48e00f108081ea2218453e25ca581e0
Kyeongho Shin <2158653@slu.edu.ph>
2019-10-15 15:34:32 +0800

    NQsolver -workign on progress

--- src/NQsolver.java
+++ src/NQsolver.java
@@ -13,12 +13,9 @@ public class NQsolver {
     public static void run()throws Exception{
         NQReader r = new NQReader("information.txt");
         NQnode initial= new NQnode(r.getWidth(),r.getHeight(),r.getNoOfQeens(),r.getBlockedBlocks());
-        ArrayList<NQnode> sides();
         int index=0;
 
-        while(!sides.isEmpty()){
 
-        }
     }
 
     public NQnode findNext(NQnode a){
@@ -27,8 +24,45 @@ public class NQsolver {
         String[][]map = a.getMap();
         ArrayList<int[]> queenList = a.getQueenLocation();
         ArrayList<int[]> blockList = a.getBlockedBlocks();
+        ArrayList<String> availMoves = new ArrayList<String>();
+        availMoves.add("N");
+        availMoves.add("NE");
+        availMoves.add("E");
+        availMoves.add("SE");
+        availMoves.add("S");
+        availMoves.add("SW");
+        availMoves.add("W");
+        availMoves.add("NW");
 
-        while
+        int x,y;
+        int[] direction = new int[2];
+        for(int i =0; i<queenList.size(); i++){
+            direction = queenList.get(i);
+            
+            //availmoves remover
+            //N-north , NE-NorthEast, E-East, SE-southeast, S-south, SW-Southwest...
+            if(direction[0]==0){
+                availMoves.remove("W");
+                availMoves.remove("NW");
+                availMoves.remove("SW");
+            }
+            if(direction[0]==map.length-1){
+                availMoves.remove("E");
+                availMoves.remove("NE");
+                availMoves.remove("SE");
+            }
+            if(direction[1]==0){
+                availMoves.remove("N");
+                availMoves.remove("NE");
+                availMoves.remove("NW");
+            }
+            if(direction[1]==map.length-1){
+                availMoves.remove("S");
+                availMoves.remove("SE");
+                availMoves.remove("SW");
+            }
+
+        }
 
     }
 }
