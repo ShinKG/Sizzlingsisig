9ec371bd2008ad04a28628560f0c26d702d6ce28
Kyeongho Shin <2158653@slu.edu.ph>
2019-10-16 14:03:15 +0800

    NQsolver -workign on progress -stucking on infinite loop

--- src/NQsolver.java
+++ src/NQsolver.java
@@ -1,8 +1,9 @@
 import java.util.ArrayList;
 public class NQsolver {
-    ArrayList<NQnode> frontiers;
-    NQnode currnet;
-    public void main(String[] args){
+    private static ArrayList<NQnode> frontiers = new ArrayList<>();
+    private static NQnode currnet;
+
+    public static void main(String[] args){
         try{
             run();
         }catch(Exception x){
@@ -14,12 +15,15 @@ public class NQsolver {
         NQReader r = new NQReader("information.txt");
         NQnode initial= new NQnode(r.getWidth(),r.getHeight(),r.getNoOfQeens(),r.getBlockedBlocks());
         int index=0;
+        frontiers.add(findNext(initial));
+        System.out.println(frontiers);
 
 
     }
 
-    public NQnode findNext(NQnode a){
-        ArrayList<NQnode> candidates;
+    public static NQnode findNext(NQnode a){
+        ArrayList<NQnode> candidates,candifinal=new ArrayList<>();
+        NQnode smallest =null;
 
         String[][]map = a.getMap();
         ArrayList<int[]> queenList = a.getQueenLocation();
@@ -30,6 +34,7 @@ public class NQsolver {
         int[] direction = new int[2];
         for(int i =0; i<queenList.size(); i++){
             direction = queenList.get(i);
+            availMoves.clear();
             availMoves.add("N");
             availMoves.add("NE");
             availMoves.add("E");
@@ -56,30 +61,128 @@ public class NQsolver {
                 availMoves.remove("NE");
                 availMoves.remove("NW");
             }
-            if(direction[1]==map.length-1){
+            if(direction[1]==map[0].length-1){
                 availMoves.remove("S");
                 availMoves.remove("SE");
                 availMoves.remove("SW");
             }
+            System.out.print("[");
+            System.out.print("("+queenList.get(i)[0]+","+queenList.get(i)[1]+")");
+            System.out.print("]");
+            System.out.println(availMoves);
 
             candidates = expands(availMoves,a,direction);
-
+            smallest = getnQnode(candidates, smallest);
+            candifinal.add(smallest);
         }
+        smallest = getnQnode(candifinal, smallest);
+        return smallest;
+    }
+
+    private static  NQnode getnQnode(ArrayList<NQnode> candifinal, NQnode smallest) {
+        for(int k=0; k<candifinal.size();k++){
+            if(smallest==null){
+                smallest = candifinal.get(k);
+                continue;
+            }
+            smallest.findConflict();
+            if(smallest.getConflict()>candifinal.get(k).getConflict()){
+                smallest = candifinal.get(k);
+            }
+        }
+        return smallest;
     }
 
     public static ArrayList<NQnode> expands(ArrayList<String> mov,NQnode can,int[] qLoc){
 
         ArrayList<NQnode> returning = new ArrayList<NQnode>();
         String[][] d_map= can.getMap();
-        ArrayList<int[]> nextQ = can.getQueenLocation();
+        ArrayList<int[]> nextQ;
         ArrayList<int[]> nextB = can.getBlockedBlocks();
 
         for(int i=0; i<mov.size(); i++) {
+            nextQ = can.getQueenLocation();
             if(mov.get(i).equals("N")){
-                d_map[qLoc[0]][qLoc[1]] = can.getMap()[qLoc[0]][qLoc[1]-1];
-                d_map[qLoc[0]][qLoc[1]-1] = can.getMap()[qLoc[0]][qLoc[1]];
-                returning.add(new NQnode(can,));
+                if(!(d_map[qLoc[0]][qLoc[1]-1].equals("B"))&&!(d_map[qLoc[0]][qLoc[1]-1].equals("Q"))) {
+                    d_map[qLoc[0]][qLoc[1]] = can.getMap()[qLoc[0]][qLoc[1] - 1];
+                    d_map[qLoc[0]][qLoc[1] - 1] = can.getMap()[qLoc[0]][qLoc[1]];
+                    nextQ.remove(qLoc);
+                    nextQ.add(new int[]{qLoc[0], qLoc[1] - 1});
+                    returning.add(new NQnode(can, d_map.length, d_map[0].length, can.getNoOfQueens(), nextQ, nextB));
+                }
             }
+            nextQ = can.getQueenLocation();
+            if(mov.get(i).equals("NE")){
+                if(!d_map[qLoc[0]+1][qLoc[1]-1].equals("B")&&!d_map[qLoc[0]+1][qLoc[1]-1].equals("Q")) {
+                    d_map[qLoc[0]][qLoc[1]] = can.getMap()[qLoc[0] + 1][qLoc[1] - 1];
+                    d_map[qLoc[0] + 1][qLoc[1] - 1] = can.getMap()[qLoc[0]][qLoc[1]];
+                    nextQ.remove(qLoc);
+                    nextQ.add(new int[]{qLoc[0] + 1, qLoc[1] - 1});
+                    returning.add(new NQnode(can, d_map.length, d_map[0].length, can.getNoOfQueens(), nextQ, nextB));
+                }
+            }
+            nextQ = can.getQueenLocation();
+            if(mov.get(i).equals("E")){
+                if(!d_map[qLoc[0]+1][qLoc[1]].equals("B")&&!d_map[qLoc[0]+1][qLoc[1]].equals("Q")) {
+                    d_map[qLoc[0]][qLoc[1]] = can.getMap()[qLoc[0] + 1][qLoc[1]];
+                    d_map[qLoc[0] + 1][qLoc[1]] = can.getMap()[qLoc[0]][qLoc[1]];
+                    nextQ.remove(qLoc);
+                    nextQ.add(new int[]{qLoc[0] + 1, qLoc[1]});
+                    returning.add(new NQnode(can, d_map.length, d_map[0].length, can.getNoOfQueens(), nextQ, nextB));
+                }
+            }
+            nextQ = can.getQueenLocation();
+            if(mov.get(i).equals("SE")){
+                if(!d_map[qLoc[0]+1][qLoc[1]+1].equals("B")&&!d_map[qLoc[0]+1][qLoc[1]+1].equals("Q")) {
+                    System.out.println("from x: " + qLoc[0] + " y: " + qLoc[1]);
+                    d_map[qLoc[0]][qLoc[1]] = can.getMap()[qLoc[0] + 1][qLoc[1] + 1];
+                    d_map[qLoc[0] + 1][qLoc[1] + 1] = can.getMap()[qLoc[0]][qLoc[1]];
+                    nextQ.remove(qLoc);
+                    nextQ.add(new int[]{qLoc[0] + 1, qLoc[1] + 1});
+                    returning.add(new NQnode(can, d_map.length, d_map[0].length, can.getNoOfQueens(), nextQ, nextB));
+                }
+            }
+            nextQ = can.getQueenLocation();
+            if(mov.get(i).equals("S")) {
+                if (!d_map[qLoc[0]][qLoc[1] + 1].equals("B") && !d_map[qLoc[0]][qLoc[1] + 1].equals("Q")) {
+                    d_map[qLoc[0]][qLoc[1]] = can.getMap()[qLoc[0]][qLoc[1] + 1];
+                    d_map[qLoc[0]][qLoc[1] + 1] = can.getMap()[qLoc[0]][qLoc[1]];
+                    nextQ.remove(qLoc);
+                    nextQ.add(new int[]{qLoc[0], qLoc[1] + 1});
+                    returning.add(new NQnode(can, d_map.length, d_map[0].length, can.getNoOfQueens(), nextQ, nextB));
+                }
+            }
+            nextQ = can.getQueenLocation();
+            if(mov.get(i).equals("SW")) {
+                if (!d_map[qLoc[0] - 1][qLoc[1] + 1].equals("B") && !d_map[qLoc[0] - 1][qLoc[1] + 1].equals("Q")) {
+                    d_map[qLoc[0]][qLoc[1]] = can.getMap()[qLoc[0] - 1][qLoc[1] + 1];
+                    d_map[qLoc[0] - 1][qLoc[1] + 1] = can.getMap()[qLoc[0]][qLoc[1]];
+                    nextQ.remove(qLoc);
+                    nextQ.add(new int[]{qLoc[0] - 1, qLoc[1] + 1});
+                    returning.add(new NQnode(can, d_map.length, d_map[0].length, can.getNoOfQueens(), nextQ, nextB));
+                }
+            }
+            nextQ = can.getQueenLocation();
+            if(mov.get(i).equals("W")){
+                if(!d_map[qLoc[0]-1][qLoc[1]].equals("B")&&!d_map[qLoc[0]-1][qLoc[1]].equals("Q")){
+                    d_map[qLoc[0]][qLoc[1]] = can.getMap()[qLoc[0]-1][qLoc[1]];
+                    d_map[qLoc[0]-1][qLoc[1]] = can.getMap()[qLoc[0]][qLoc[1]];
+                    nextQ.remove(qLoc);
+                    nextQ.add(new int[]{qLoc[0]-1,qLoc[1]});
+                    returning.add(new NQnode(can,d_map.length,d_map[0].length,can.getNoOfQueens(),nextQ,nextB));
+                }
+            }
+            if(mov.get(i).equals("NW")) {
+                if (!d_map[qLoc[0] - 1][qLoc[1] - 1].equals("B") && !d_map[qLoc[0] - 1][qLoc[1] - 1].equals("Q")) {
+                    d_map[qLoc[0]][qLoc[1]] = can.getMap()[qLoc[0] - 1][qLoc[1] - 1];
+                    d_map[qLoc[0] - 1][qLoc[1] - 1] = can.getMap()[qLoc[0]][qLoc[1]];
+                    nextQ.remove(qLoc);
+                    nextQ.add(new int[]{qLoc[0] - 1, qLoc[1] - 1});
+                    returning.add(new NQnode(can, d_map.length, d_map[0].length, can.getNoOfQueens(), nextQ, nextB));
+                }
+            }
+
         }
+        return returning;
     }
 }
